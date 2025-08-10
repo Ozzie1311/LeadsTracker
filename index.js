70e6f4d15ca5d08e7d6cc58671ec52d989d917fd
Oswaldo <rodriosw13@gmail.com>
2025-08-10 10:30:45 -0400

    Adding firebase

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js'
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js'
+
+const firebaseConfig = {}
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+const refInDB = ref(database, 'leads')
+
 let myLeads = []
 const inputEl = document.getElementById('input-el')
 const inputBtn = document.getElementById('input-btn')
 const ulEl = document.getElementById('ul-el')
 const deleteBtn = document.getElementById('delete-btn')
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
-const tabBtn = document.getElementById('tab-btn')
 
-if (leadsFromLocalStorage) {
-  myLeads = leadsFromLocalStorage
-  render(myLeads)
-}
+// tabBtn.addEventListener('click', function () {
+//   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//     myLeads.push(tabs[0].url)
+//     localStorage.setItem('myLeads', JSON.stringify(myLeads))
+//     render(myLeads)
+//   })
+// })
 
-tabBtn.addEventListener('click', function () {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    myLeads.push(tabs[0].url)
-    localStorage.setItem('myLeads', JSON.stringify(myLeads))
-    render(myLeads)
-  })
+onValue(refInDB, function (snapshot) {
+  const snapshotDoesExist = snapshot.exists() //Con esto verificamos si la captura de la referencia existe
+  if (snapshotDoesExist) {
+    const snapshotValues = snapshot.val()
+    const leads = Object.values(snapshotValues)
+    render(leads)
+  }
+  return //Sí no existe, no hacemos nada
 })
 
 function render(leads) {
@@ -34,14 +52,11 @@ function render(leads) {
 }
 
 deleteBtn.addEventListener('dblclick', function () {
-  localStorage.clear()
-  myLeads = []
-  render(myLeads)
+  remove(refInDB) //Método para eliminar completamente la referencia de la bd
+  ulEl.innerHTML = ''
 })
 
 inputBtn.addEventListener('click', function () {
-  myLeads.push(inputEl.value)
+  push(refInDB, inputEl.value) //Método para ingresar los valores del input a la bd
   inputEl.value = ''
-  localStorage.setItem('myLeads', JSON.stringify(myLeads))
-  render(myLeads)
 })
